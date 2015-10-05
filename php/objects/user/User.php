036542f7c95acbae048a559a47d24ad5e0ff598d
Marissa Schmidt <marissaschmidt@boisestate.edu>
2015-10-04 18:11:29 -0600

    Adding updated class example

--- php/objects/user/User.php
+++ php/objects/user/User.php
@@ -7,12 +7,33 @@
 class User {
   // Fields (can be public or private)
   // id, email, password
+  private $id;
+  private $email;
+  private $password;
 
   // Magic Functions
-  // __constructor (must use $this->field)
-  // __toString
+  // __construct (must use $this->field)
+  public function __construct($id = 0, $email = "", $password = "")
+  {
+    $this->id = $id;
+    $this->email = $email;
+    $this->password = $password;
+  }
+  //__toString
+  public function __toString()
+  {
+    return "[id=$this->id, email=$this->email]";
+  }
 
   // Methods (can be public or private)
   // validatePassword
+  public function validatePassword($password)
+  {
+    if($this->password === $password)
+    {
+      return true;
+    }
+      return false;
+  }
 }
 ?>
--- php/objects/user/UserTest.php
+++ php/objects/user/UserTest.php
@@ -2,8 +2,7 @@
   include_once('User.php');
 
   $user = new User();
-
-  // $user1 = new User(123, "snoopy@gmail.com", "thisi5s3cur3");
+  $user1 = new User(123, "snoopy@gmail.com", "thisi5s3cur3");
   // $user2 = new User(456, "cinderella@gmail.com", "pumpkin", "Cinderella");
 ?>
 <html>
@@ -11,15 +10,15 @@
 <body>
   <pre>
 <?php
-  var_dump($user);
+  //var_dump($user);
+  //var_dump($user1);
+  echo "$user\n"; // uses __toString method
 
-  //echo "$user\n"; // uses __toString method
-
-  // if($user1->validatePassword("thisi5s3cur3")) {
-  //   echo "Passwords match. Welcome!";
-  // } else {
-  //   echo "Invalid password. Please try again.";
-  // }
+  if($user1->validatePassword("thisi5s3cur3")) {
+    echo "Passwords match. Welcome!";
+  } else {
+    echo "Invalid password. Please try again.";
+  }
 ?>
 </pre>
 </body>
