a874c6e18eba7a0308c9bde32f95c39307263891
Marissa R. Schmidt <marissaschmidt@boisestate.edu>
2015-11-11 16:37:22 -0700

    demo ui plugin

--- javascript/jquery/matrix.js
+++ javascript/jquery/matrix.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
 	//Simple mouseenter handler that accesses the elements via table element IDs
-	$('td').on('mouseenter', function() {
-		alert("You entered the matrix at element " + $(this).text() + "!");
-	});
+	//$('td').on('mouseenter', function() {
+	//	alert("You entered the matrix at element " + $(this).text() + "!");
+	//});
 
 	// Or could use hover
-	//$("td").hover(function(){
-	//	$(this).toggleClass("redpill");
-	//}, function(){
-	//	$(this).toggleClass("redpill");
-	//});
+	$("td").hover(function(){
+		$(this).toggleClass("redpill");
+	}, function(){
+		$(this).toggleClass("redpill");
+	});
+
+	$(function() {
+		$( "#draggable" ).draggable();
+	});
 });
 
