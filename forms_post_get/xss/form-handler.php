b924f563c341260e86e6a7258c195583f1c99392
Marissa R. Schmidt <marissaschmidt@boisestate.edu>
2016-06-07 11:47:21 -0600

    Added new XSS example

--- forms_post_get/xss/form-handler.php
+++ forms_post_get/xss/form-handler.php
@@ -2,24 +2,21 @@
 	<head>
 		<title>XSS Demo</title>
 	</head>
-<body>
-
-<?php
-if($_SERVER['REQUEST_METHOD'] == "POST")
-{
-		$firstName = $_POST['firstName'];
-		$lastName = $_POST['lastName'];
-?>
-	<!-- Print received values -->
-	<div>
-		<p>Received the following results:</p>
-		<p>First Name: "<?= $firstName; ?>"</p>
-		<p>Last Name: "<?= $lastName; ?>"</p>
-	</div>
-<?php
-} else {
-	header('Location: form.html');
-} ?>
-
-</body>
+	<body>
+	<?php
+	if($_SERVER['REQUEST_METHOD'] == "POST") {
+			$firstName = $_POST['firstName'];
+			$lastName = $_POST['lastName'];
+	?>
+		<section id="results">
+		<h1>Results</h1>
+		<div>
+			<p>First Name: "<?= $firstName; ?>"</p>
+			<p>Last Name: "<?= $lastName; ?>"</p>
+		</div>
+		</section>
+	<?php } else {
+		header('Location: form.html');
+	} ?>
+	</body>
 </html>
