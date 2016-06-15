44ce22d231858f0901619592474d18f4e0035465
Marissa R. Schmidt <marissaschmidt@boisestate.edu>
2016-06-15 14:29:35 -0600

    Adding better name list.

--- javascript/ajax/autocomplete/form-hint.php
+++ javascript/ajax/autocomplete/form-hint.php
@@ -1,52 +1,24 @@
 <?php
-// Array with names (Modified from w3c)
-$a[] = "Anna";
-$a[] = "Brittany";
-$a[] = "Cinderella";
-$a[] = "Diana";
-$a[] = "Eva";
-$a[] = "Fiona";
-$a[] = "Gunda";
-$a[] = "Hege";
-$a[] = "Inga";
-$a[] = "Johanna";
-$a[] = "Kitty";
-$a[] = "Linda";
-$a[] = "Nina";
-$a[] = "Ophelia";
-$a[] = "Petunia";
-$a[] = "Amanda";
-$a[] = "Raquel";
-$a[] = "Cindy";
-$a[] = "Doris";
-$a[] = "Eve";
-$a[] = "Evita";
-$a[] = "Sunniva";
-$a[] = "Tove";
-$a[] = "Unni";
-$a[] = "Violet";
-$a[] = "Liza";
-$a[] = "Elizabeth";
-$a[] = "Ellen";
-$a[] = "Wenche";
-$a[] = "Vicky";
+$names = file("first-names.txt");
 
 // get the q parameter from URL
-$q = $_GET["q"];
+if(isset($_GET["q"])) {
+	$q = $_GET["q"];
 
-$hints = array("hints" => array());
+	$hints = array("hints" => array());
 
-# lookup all hints from array if $q is different from ""
-if ($q !== "") {
-    $q = strtolower($q);
-    $len=strlen($q);
-    foreach($a as $name) {
-      if (stristr($q, substr($name, 0, $len))) {
-          $hints["hints"][] = $name;
-        }
-    }
+	# lookup all hints from array if $q is different from ""
+	if ($q !== "") {
+		$q = strtolower($q);
+		$len=strlen($q);
+		foreach($names as $name) {
+		  if (stristr($q, substr($name, 0, $len))) {
+			  $hints["hints"][] = $name;
+			}
+		}
+	}
+	// Output "no suggestion" if no hint was found or output correct values
+	//$return = array( "hint" => $hint === "" ? "no suggestion" : $hint);
+	echo json_encode($hints);
 }
-// Output "no suggestion" if no hint was found or output correct values
-//$return = array( "hint" => $hint === "" ? "no suggestion" : $hint);
-echo json_encode($hints);
 ?>
--- javascript/ajax/autocomplete/script.js
+++ javascript/ajax/autocomplete/script.js
@@ -29,4 +29,3 @@ function showHint(str) {
 			});
 		}
 }
-
