b41ba88f69fab2a7f42ab74f30bafd762201fbe5
Marissa Schmidt <marissaschmidt@boisestate.edu>
2017-09-25 13:18:43 -0600

    Cleanup

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
+<html lang="en">
+<head>
+  <title>CS401 Examples</title>
+  <link href="style.css" type="text/css" rel="stylesheet">
+  <meta charset="UTF-8">
+</head>
+<body>
+<div class="flex-container">
 <?php
-	echo 'This is a test';
+  // $dirs = glob("*", GLOB_ONLYDIR);
+  $dirs = array("html-css", "javascript", "php", "templating", "forms-post-get", "sql", "apis", "static-website", "sample-website");
+  foreach($dirs as $dir) {
+?>
+	<a class="flex-item" href="<?= $dir ?>"><?= $dir ?></a> 
+<?php
+  }
+?>
+</div>
+</body>
+</html>
