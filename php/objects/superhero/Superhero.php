24f3eb473f56b0a261fd1d74f6a8f31aaec6da1b
Marissa Schmidt <marissaschmidt@boisestate.edu>
2019-09-25 12:30:55 -0600

    Fixed indentation

--- php/objects/superhero/Superhero.php
+++ php/objects/superhero/Superhero.php
@@ -7,10 +7,10 @@ include('SuperheroInterface.php');
  */
 class Superhero implements SuperheroInterface
 {
-	private $firstName;
-	private $lastName;
-	private $alias;
-	private $abilities;
+  private $firstName;
+  private $lastName;
+  private $alias;
+  private $abilities;
 
   /**
    * Constructor. A "magic" function.
@@ -19,76 +19,76 @@ class Superhero implements SuperheroInterface
    * @param string $alias
    * @param array $abilities
    */
-	public function __construct($firstName, $lastName, $alias, $abilities = array())
-	{
-		$this->firstName = $firstName;
-		$this->lastName = $lastName;
-		$this->alias = $alias;
-		$this->abilities = $abilities;
-	}
+  public function __construct($firstName, $lastName, $alias, $abilities = array())
+  {
+    $this->firstName = $firstName;
+    $this->lastName = $lastName;
+    $this->alias = $alias;
+    $this->abilities = $abilities;
+  }
 
   /**
    * Sets the first and last name of the superhero
    * @param string $firstName
    * @param string $lastName
    */
-	public function setFullName($firstName, $lastName) {
-		$this->firstName = $firstName;
-		$this->lastName = $lastName;
-	}
+  public function setFullName($firstName, $lastName) {
+    $this->firstName = $firstName;
+    $this->lastName = $lastName;
+  }
 
   /**
    * Returns the full name.
    * @return string The full name.
    */
-	public function getFullName() {
-		return $this->firstName . " " . $this->lastName;
-	}
+  public function getFullName() {
+    return $this->firstName . " " . $this->lastName;
+  }
 
   /**
    * Sets the alias of the superhero
    * @param string $alias
    */
-	public function setAlias($alias) {
-		$this->alias = $alias;
-	}
+  public function setAlias($alias) {
+    $this->alias = $alias;
+  }
   /**
    * Returns the alias.
    * @return string The alias.
    */
-	public function getAlias() {
-		return $this->alias;
-	}
+  public function getAlias() {
+    return $this->alias;
+  }
 
   /**
    * Adds ability to the list of abilities
    * @param string $ability
    */
-	public function addAbility($ability) {
-		$this->abilities[] = $ability;
-	}
+  public function addAbility($ability) {
+    $this->abilities[] = $ability;
+  }
 
   /**
    * Returns the list of abilities.
    * @return array The list of abilities.
    */
-	public function getAbilities() {
-		return $this->abilities;
-	}
+  public function getAbilities() {
+    return $this->abilities;
+  }
 
   /*
    * Returns the initials of first and last name
    * @return string The initials (e.g. B.B.)
    */
-	public function getInitials() {
-		return $this->firstName[0] . "." . $this->lastName[0] . ".";
-	}
+  public function getInitials() {
+    return $this->firstName[0] . "." . $this->lastName[0] . ".";
+  }
 
   /**
    * Returns the full name and alias of the superhero.
    * @return string The fullname and alias
    */
-	public function __toString() {
-      return $this->getFullName() . ' (' . $this->alias . ')';
-    }
+  public function __toString() {
+    return $this->getFullName() . ' (' . $this->alias . ')';
+  }
 }
--- php/objects/superhero/SuperheroInterface.php
+++ php/objects/superhero/SuperheroInterface.php
@@ -12,43 +12,43 @@ interface SuperheroInterface
    * @param string $firstName
    * @param string $lastName
    */
-	public function setFullName($firstName, $lastName); 
+  public function setFullName($firstName, $lastName); 
 
   /**
    * Returns the full name.
    * @return string The full name.
    */
-	public function getFullName(); 
+  public function getFullName(); 
 
   /**
    * Sets the alias of the superhero
    * @param string $alias
    */
-	public function setAlias($alias); 
+  public function setAlias($alias); 
 
   /**
    * Returns the alias.
    * @return string The alias.
    */
-	public function getAlias(); 
+  public function getAlias(); 
 
   /**
    * Adds ability to the list of abilities
    * @param string $ability
    */
-	public function addAbility($ability); 
+  public function addAbility($ability); 
 
   /**
    * Returns the list of abilities.
    * @return array The list of abilities.
    */
-	public function getAbilities(); 
+  public function getAbilities(); 
 
   /*
    * Returns the initials of first and last name
    * @return string The initials (e.g. B.B.)
    */
-	public function getInitials(); 
+  public function getInitials(); 
 
   // Don't forget to define your toString
 
