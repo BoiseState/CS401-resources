ab6fe771a80f15549ae6d30c043ab1d60baec1fa
Marissa Schmidt <marissaschmidt@boisestate.edu>
2019-10-15 15:19:14 -0600

    Re-importing database/user creation scripts

---
+++ sql/create-webdev-db.sql
@@ -0,0 +1,8 @@
+-- To run this script, use the following command 
+-- mysql -u root -p < create-webdev-db.sql
+--     (enter root password when prompted)
+
+DROP DATABASE IF EXISTS webdev;
+CREATE DATABASE webdev;
+
+SHOW DATABASES;
