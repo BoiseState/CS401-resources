4f3df39091a0624bcc4371d9225a5ae0dc418393
Marissa Schmidt <marissaschmidt@boisestate.edu>
2019-11-06 12:04:54 -0700

    Updated speech synthesis js example

--- javascript/events/magicword.js
+++ javascript/events/magicword.js
@@ -1,8 +1,10 @@
-
 /**
  * This is the JavaScript companion to "sayMagicWord.html",
  * which contains the functions for the event handlers.
  */
+
+const speakButton = document.querySelector('#speak'); 
+speakButton.addEventListener('click', speakMagicWord);
 
 /**
  * Function: Text Magic Word
@@ -16,12 +18,15 @@ function textMagicWord() {
  * Function: Speak Magic Word
  * Description: Uses a speech synthesis object to verbally speak a word.
  */
-function speakMagicWordSpeak() {
-	/** Initialize and configure speech synthesis object */
-	var msg = new SpeechSynthesisUtterance();
-	var voices = window.speechSynthesis.getVoices();
-	msg.voice = voices[10]; // Note: some voices don't support altering params
-	msg.voiceURI = 'native';
+function speakMagicWord()
+{
+	// Initialize and configure speech synthesis object.
+	let msg = new SpeechSynthesisUtterance();
+	let voices = window.speechSynthesis.getVoices();
+
+  // Note: some voices don't support altering params
+	msg.voice = voices[10];
+  msg.voiceURI = 'native';
 	msg.volume = 1; // 0 to 1
 	msg.rate = 1; // 0.1 to 10
 	msg.pitch = 2; //0 to 2
